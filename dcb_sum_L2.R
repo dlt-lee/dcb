337e8cdbd6576879bae3f44025f6f25bb370a1e2
dlt-lee <664219378@qq.com>
2020-06-11 02:17:52 +0800

    modify

--- dcb_sum_L2.R
+++ dcb_sum_L2.R
@@ -1,20 +1,20 @@
-dcb_sum_L2<-function(data_org) {
+#dcb_sum_L2<-function(data_org) {
   library('xgboost')
   source("dcb_sum_L1.R")
-  
+  data_org<-dcb
   rows<-dim(data_org)[1]
-  line<-rows-309
+  line<-rows%%3 +15
   j<-1
   a1_temp<-c(0,0,0,0,0,0,0,0)
   a2_temp<-c(0,0,0,0,0,0,0,0)
   a3_temp<-c(0,0,0,0,0,0,0,0)
   a4_temp<-c(0,0,0,0,0,0,0,0)
   a5_temp<-c(0,0,0,0,0,0,0,0)
+  a6_temp<-c(0,0,0,0,0,0,0,0)
   b1_temp<-c(0,0,0,0,0,0,0,0)
-  b2_temp<-c(0,0,0,0,0,0,0,0)
   res_temp<-c(0,0,0,0,0,0,0)
   
-  for (i in 1:line) {
+  for (i in 1:rows) {
     data<-data_org[j:line,]
     temp<-dcb_sum_L1(data)
     a1_temp<-c(a1_temp,temp$a1)
@@ -22,16 +22,19 @@ dcb_sum_L2<-function(data_org) {
     a3_temp<-c(a3_temp,temp$a3)
     a4_temp<-c(a4_temp,temp$a4)
     a5_temp<-c(a5_temp,temp$a5)
-    a6_temp<-c(b1_temp,temp$a6)
-    b1_temp<-c(b2_temp,temp$b1)
+    a6_temp<-c(a6_temp,temp$a6)
+    b1_temp<-c(b1_temp,temp$b1)
     res_temp<-c(res_temp,data_org[line+1,2:8])
-    j<j+3
+    j<-j+3
     line<-line+3
     if (line>=rows) {
       break
     }
     
   }
+  
+  print(line)
+  
   a1_m<-matrix(a1_temp,ncol = 8,byrow = TRUE)[-1,]
   a2_m<-matrix(a2_temp,ncol = 8,byrow = TRUE)[-1,]
   a3_m<-matrix(a3_temp,ncol = 8,byrow = TRUE)[-1,]
@@ -59,7 +62,7 @@ dcb_sum_L2<-function(data_org) {
   
   
   #prediect
-  tests<-dlt_sum_L1(tail(data_org,309))
+  tests<-dcb_sum_L1(tail(data_org,15))
   tests.T<-Matrix(as.matrix(tests),sparse=T)
   testPredictions.a1 <- predict(object = bst.a1,newdata = t(tests.T[,1]))
   testPredictions.a2 <- predict(object = bst.a2,newdata = t(tests.T[,2]))
@@ -82,7 +85,7 @@ dcb_sum_L2<-function(data_org) {
   
   
   
-}
+#}
 
-
+ 
 
