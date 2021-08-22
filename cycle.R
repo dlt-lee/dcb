ff18b2cef25bd878c344f4d8d26ae9a308541afb
dcb <li.oyhz@foxmail.com>
2021-08-22 22:45:20 +0800

    19001-19151

--- cycle.R
+++ cycle.R
@@ -9,7 +9,7 @@ source("dcb_L8.R")
 source("dcb_L9.R")
 source("dcb_JL.R")
 
-for (i in 18001:18153) {
+for (i in 19001:19151) {
   # dcb_L2(i)
   dcb_L3(i,-1)
   dcb_L4(i,-1)
@@ -17,7 +17,7 @@ for (i in 18001:18153) {
   dcb_L6(i,-1)
   dcb_L7(i,-1)
   dcb_L8(i,-1)
-  # dcb_L9(i,-1)
+  dcb_L9(i,-1)
   # dcb_JL(i,-1)
 }
 
