826b8c86c8cfb835141c6d9527daa803e8bdf499
zhuo.lee <li.ouhz@foxmail.com>
2021-08-23 22:37:34 +0800

    update cycle

--- cycle.R
+++ cycle.R
@@ -9,6 +9,64 @@ source("dcb_L8.R")
 source("dcb_L9.R")
 source("dcb_JL.R")
 
+
+data_year = c(
+  07001,07153,
+  08001,08153,
+  09001,09154,
+  10001,10153,
+  11001,11153,
+  12001,12154,
+  13001,13154,
+  14001,14152,
+  15001,15154,
+  16001,16153,
+  17001,17154,
+  18001,18153,
+  19001,19151,
+  20001,20134,
+  21001,21095
+  
+)
+length(data_year)
+
+data_year=matrix(data_year,ncol = 2,byrow = TRUE,dimnames = NULL)
+
+rows=dim(data_year)[1]
+
+for (i in 1:rows) {
+  for (j in data_year[i,1]:data_year[i,2]) {
+    if (i>=1) {
+      dcb_L3(j,number_of_core)
+    }
+    if (i>=3) {
+      dcb_L4(j,number_of_core)
+    }
+    if (i>=5) {
+      dcb_L5(j,number_of_core)
+    }
+    if (i>=7) {
+      dcb_L6(j,number_of_core)
+    }
+    if (i>=9) {
+      dcb_L7(j,number_of_core)
+    }
+    if (i>=11) {
+      dcb_L8(j,number_of_core)
+    }
+    if (i>=13) {
+      dcb_L9(j,number_of_core)
+    }
+    if (i>=15) {
+      dcb_JL(j,number_of_core)
+    }
+
+  }
+  
+}
+
+
+
 for (i in 19001:19151) {
   # dcb_L2(i)
   dcb_L3(i,-1)
