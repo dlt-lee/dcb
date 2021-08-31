74bd7529cfd6a79dd4381171aae097e53550e6df
dlt-lee <li.ouhz@foxmail.com>
2021-08-31 16:55:38 +0800

    update JL data

--- cycle.R
+++ cycle.R
@@ -102,18 +102,18 @@ source("dcb_L18.R")
 # }
 
 
-# 
-# for (i in 19001:19151) {
-#   # dcb_L2(i)
-#   dcb_L3(i,-1)
-#   dcb_L4(i,-1)
-#   dcb_L5(i,-1)
-#   dcb_L6(i,-1)
-#   dcb_L7(i,-1)
-#   dcb_L8(i,-1)
-#   dcb_L9(i,-1)
-#   # dcb_JL(i,-1)
-# }
+
+for (i in 21001:21098) {
+  # dcb_L2(i)
+  # dcb_L3(i,-1)
+  # dcb_L4(i,-1)
+  # dcb_L5(i,-1)
+  # dcb_L6(i,-1)
+  # dcb_L7(i,-1)
+  # dcb_L8(i,-1)
+  # dcb_L9(i,-1)
+  dcb_JL(i,-1)
+}
 
 time_start_dcb<-Sys.time()
 
