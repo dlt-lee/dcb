54166602256e1e8c983b27a445c9a5d51b834fe3
dlt-lee <664219378@qq.com>
2021-09-11 08:07:09 +0800

    21103

--- cycle.R
+++ cycle.R
@@ -21,60 +21,60 @@ source("dcb_L18.R")
 
 
 
-data_year = c(
-  07001,07153,
-  08001,08153,
-  09001,09154,
-  10001,10153,
-  11001,11153,
-  12001,12154,
-  13001,13154,
-  14001,14152,
-  15001,15154,
-  16001,16153,
-  17001,17154,
-  18001,18153,
-  19001,19151,
-  20001,20134,
-  21001,21102
-
-)
-length(data_year)
-
-data_year=matrix(data_year,ncol = 2,byrow = TRUE,dimnames = NULL)
-
-rows=dim(data_year)[1]
-
-for (i in 1:rows) {
-  for (j in data_year[i,1]:data_year[i,2]) {
-    if (i>=1) {
-      dcb_L3(j,number_of_core)
-    }
-    if (i>=3) {
-      dcb_L4(j,number_of_core)
-    }
-    if (i>=5) {
-      dcb_L5(j,number_of_core)
-    }
-    if (i>=7) {
-      dcb_L6(j,number_of_core)
-    }
-    if (i>=9) {
-      dcb_L7(j,number_of_core)
-    }
-    if (i>=11) {
-      dcb_L8(j,number_of_core)
-    }
-    if (i>=13) {
-      dcb_L9(j,number_of_core)
-    }
-    if (i>=15) {
-      dcb_JL(j,number_of_core)
-    }
-
-  }
-
-}
+# data_year = c(
+#   07001,07153,
+#   08001,08153,
+#   09001,09154,
+#   10001,10153,
+#   11001,11153,
+#   12001,12154,
+#   13001,13154,
+#   14001,14152,
+#   15001,15154,
+#   16001,16153,
+#   17001,17154,
+#   18001,18153,
+#   19001,19151,
+#   20001,20134,
+#   21001,21102
+# 
+# )
+# length(data_year)
+# 
+# data_year=matrix(data_year,ncol = 2,byrow = TRUE,dimnames = NULL)
+# 
+# rows=dim(data_year)[1]
+# 
+# for (i in 1:rows) {
+#   for (j in data_year[i,1]:data_year[i,2]) {
+#     if (i>=1) {
+#       dcb_L3(j,number_of_core)
+#     }
+#     if (i>=3) {
+#       dcb_L4(j,number_of_core)
+#     }
+#     if (i>=5) {
+#       dcb_L5(j,number_of_core)
+#     }
+#     if (i>=7) {
+#       dcb_L6(j,number_of_core)
+#     }
+#     if (i>=9) {
+#       dcb_L7(j,number_of_core)
+#     }
+#     if (i>=11) {
+#       dcb_L8(j,number_of_core)
+#     }
+#     if (i>=13) {
+#       dcb_L9(j,number_of_core)
+#     }
+#     if (i>=15) {
+#       dcb_JL(j,number_of_core)
+#     }
+# 
+#   }
+# 
+# }
 
 
 # 
