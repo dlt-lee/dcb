1660a48c0a352748efdabed0f5236bed75be9ae9
leezh <664219378@qq.com>
2021-10-08 22:31:12 +0800

    21115

--- cycle.R
+++ cycle.R
@@ -66,86 +66,86 @@ source("result_check_JL_L17.R")
 
 
 
-data_year = c(
-  06001,06154,
-  07001,07153,
-  08001,08153,
-  09001,09154,
-  10001,10153,
-  11001,11153,
-  12001,12154,
-  13001,13154,
-  14001,14152,
-  15001,15154,
-  16001,16153,
-  17001,17154,
-  18001,18153,
-  19001,19151,
-  20001,20134,
-  21001,21113
-
-
-)
-length(data_year)
-
-data_year=matrix(data_year,ncol = 2,byrow = TRUE,dimnames = NULL)
-rows=dim(data_year)[1]
-
-for (i in 1:rows) {
-  for (j in data_year[i,1]:data_year[i,2]) {
-    if (i>=1) {
-      dcb_L3(j,number_of_core)
-    }
-    if (i>=2) {
-      dcb_L4(j,number_of_core)
-    }
-    if (i>=3) {
-      dcb_L5(j,number_of_core)
-    }
-    if (i>=4) {
-      dcb_L6(j,number_of_core)
-    }
-    if (i>=5) {
-      dcb_L7(j,number_of_core)
-    }
-    if (i>=6) {
-      dcb_L8(j,number_of_core)
-    }
-    if (i>=7) {
-      dcb_L9(j,number_of_core)
-    }
-    if (i>=8) {
-      dcb_L10(j,number_of_core)
-    }
-    if (i>=9) {
-      dcb_L11(j,number_of_core)
-    }
-    if (i>=10) {
-      dcb_L12(j,number_of_core)
-    }
-    if (i>=11) {
-      dcb_L13(j,number_of_core)
-    }
-    if (i>=12) {
-      dcb_L14(j,number_of_core)
-    }
-    if (i>=13) {
-      dcb_L15(j,number_of_core)
-    }
-    if (i>=14) {
-      dcb_L16(j,number_of_core)
-    }
-    if (i>=15) {
-      dcb_L17(j,number_of_core)
-    }
-    if (i>=16) {
-      dcb_L18(j,number_of_core)
-      dcb_JL(j,number_of_core)
-    }
-
-  }
-
-}
+# data_year = c(
+#   06001,06154,
+#   07001,07153,
+#   08001,08153,
+#   09001,09154,
+#   10001,10153,
+#   11001,11153,
+#   12001,12154,
+#   13001,13154,
+#   14001,14152,
+#   15001,15154,
+#   16001,16153,
+#   17001,17154,
+#   18001,18153,
+#   19001,19151,
+#   20001,20134,
+#   21001,21113
+# 
+# 
+# )
+# length(data_year)
+# 
+# data_year=matrix(data_year,ncol = 2,byrow = TRUE,dimnames = NULL)
+# rows=dim(data_year)[1]
+# 
+# for (i in 1:rows) {
+#   for (j in data_year[i,1]:data_year[i,2]) {
+#     if (i>=1) {
+#       dcb_L3(j,number_of_core)
+#     }
+#     if (i>=2) {
+#       dcb_L4(j,number_of_core)
+#     }
+#     if (i>=3) {
+#       dcb_L5(j,number_of_core)
+#     }
+#     if (i>=4) {
+#       dcb_L6(j,number_of_core)
+#     }
+#     if (i>=5) {
+#       dcb_L7(j,number_of_core)
+#     }
+#     if (i>=6) {
+#       dcb_L8(j,number_of_core)
+#     }
+#     if (i>=7) {
+#       dcb_L9(j,number_of_core)
+#     }
+#     if (i>=8) {
+#       dcb_L10(j,number_of_core)
+#     }
+#     if (i>=9) {
+#       dcb_L11(j,number_of_core)
+#     }
+#     if (i>=10) {
+#       dcb_L12(j,number_of_core)
+#     }
+#     if (i>=11) {
+#       dcb_L13(j,number_of_core)
+#     }
+#     if (i>=12) {
+#       dcb_L14(j,number_of_core)
+#     }
+#     if (i>=13) {
+#       dcb_L15(j,number_of_core)
+#     }
+#     if (i>=14) {
+#       dcb_L16(j,number_of_core)
+#     }
+#     if (i>=15) {
+#       dcb_L17(j,number_of_core)
+#     }
+#     if (i>=16) {
+#       dcb_L18(j,number_of_core)
+#       dcb_JL(j,number_of_core)
+#     }
+# 
+#   }
+# 
+# }
 
 
 
